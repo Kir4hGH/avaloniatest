045787c28ca9241b391d16cb77b81fb7272bd90e
kir4h <kpotutinskiy@mail.ru>
2026-04-01 18:03:40 +0700

    Removed unused Assets folder

--- MyApp/obj/Debug/net10.0/MyApp.AssemblyInfo.cs
+++ MyApp/obj/Debug/net10.0/MyApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06a16b5bb039cfcca54e55a72f4065d241825a2a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
